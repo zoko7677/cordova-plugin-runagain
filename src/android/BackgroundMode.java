8400a7d6c25c37bfea32bb6dd42355f914a1b512
Minh Hoang <hoangminhweb@gmail.com>
2016-12-05 15:46:52 +0700

    Update BackgroundMode.java

--- src/android/BackgroundMode.java
+++ src/android/BackgroundMode.java
@@ -137,10 +137,10 @@ public class BackgroundMode extends CordovaPlugin {
         
         // Starting your app...
         //Log.d("Cordova AppStarter", "STARTING APP...");
-        //SharedPreferences sp = mContext.getSharedPreferences("BackgroundMode", Context.MODE_PRIVATE);
+        SharedPreferences sp = mContext.getSharedPreferences("BackgroundMode", Context.MODE_PRIVATE);
         String packageName = mContext.getPackageName();
 	webView.loadUrl("javascript:alert('"+packageName+"');");
-        //String className = sp.getString("class", "");
+        String className = sp.getString("class", "");
         /*if( !className.equals("") ){
             //Log.d("Cordova AppStarter", className);
             Intent serviceIntent = new Intent();
