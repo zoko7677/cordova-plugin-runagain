f14de3cfab775410ba39e9363c0a2769c4871d82
Minh Hoang <hoangminhweb@gmail.com>
2017-03-01 15:31:21 +0700

    Update ForegroundService.java

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -48,7 +48,7 @@ import java.lang.reflect.Field;
  * something the user is actively aware of and thus not a candidate for killing
  * when low on memory.
  */
-public class ForegroundService extends Service, CordovaPlugin  {
+public class ForegroundService extends Service  {
 
     // Fixed ID for the 'foreground' notification
     public static final int NOTIFICATION_ID = -574543954;
