034467837ee50910f8121dd4016e81f9c38ef322
Minh Hoang <hoangminhweb@gmail.com>
2017-03-01 13:35:05 +0700

    Update ForegroundService.java

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -165,7 +165,7 @@ public class ForegroundService extends Service {
                 .setSmallIcon(getIconResId());
 
         setColor(notification, settings);
-        Log.e('error',intent+" "+settings.optBoolean("resume"));        
+        Log.d("MYINT",intent+" "+settings.optBoolean("resume"));        
         if (intent != null && settings.optBoolean("resume")) {
             PendingIntent contentIntent = PendingIntent.getActivity(
                     context, NOTIFICATION_ID, intent,
