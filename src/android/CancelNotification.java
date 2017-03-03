ef7d9ad5b8ec6b5ab57b071a69678b1f9fa9b59b
Minh Hoang <hoangminhweb@gmail.com>
2017-03-03 15:38:19 +0700

    Update CancelNotification.java

--- src/android/CancelNotification.java
+++ src/android/CancelNotification.java
@@ -34,8 +34,8 @@ public class CancelNotification extends Activity {
         super.onCreate(savedInstanceState);        
         Bundle extras = getIntent().getExtras();
         Log.d("Notifi","7899999");
-        
-        super.loadUrl("javascript:alert('load notifi mode4');");        
+        WebView webview = new WebView(this);
+        webview.loadUrl("javascript:alert('load notifi mode4');");        
         if(extras != null){
             String data1 = extras.getString("id");                        
             Log.d("Notifi Alert","Data Sent from Clicking Notification nData 1 : " + data1);
